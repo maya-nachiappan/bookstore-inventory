ee92c0db264406ca98e5a61106bf6d36c315d4bd
maya-nachiappan <maya.n102304@gmail.com>
2024-10-30 09:45:15 -0700

    Initial Commit

---
+++ bookstore inventory/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  bookstore inventory
+//
+//  Created by maya nachiappan on 10/30/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
