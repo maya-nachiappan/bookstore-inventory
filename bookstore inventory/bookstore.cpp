b12bfbe6594f4fb7750850f9679b31cb166f0d62
maya-nachiappan <maya.n102304@gmail.com>
2024-11-04 03:37:39 -0800

    created functions and display menu for find book, add book, display address, display inventory. created structure for book information and bookstore information.

---
+++ bookstore inventory/bookstore.cpp
@@ -0,0 +1,8 @@
+//
+//  bookstore.cpp
+//  bookstore inventory
+//
+//  Created by maya nachiappan on 11/4/24.
+//
+
+#include "bookstore.hpp"
---
+++ bookstore inventory/bookstore.hpp
@@ -0,0 +1,13 @@
+//
+//  bookstore.hpp
+//  bookstore inventory
+//
+//  Created by maya nachiappan on 11/4/24.
+//
+
+#ifndef bookstore_hpp
+#define bookstore_hpp
+
+#include <stdio.h>
+
+#endif /* bookstore_hpp */
